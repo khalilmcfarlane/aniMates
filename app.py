a89f31a0c47077dd11d42023ae6c3a040b523116
Khalil McFarlane <kmac36960@gmail.com>
2022-06-02 20:14:33 -0400

    Log out/login working

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, render_template, request, session
+from flask import Flask, redirect, render_template, request, session, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 import models
 import settings
@@ -31,23 +31,37 @@ def register():
     return render_template("signup.html")
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     if "username" in session:
-        pass
+        redirect(url_for('profile'))
         # send to profile page
     # else login 
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
 
-    username = request.form['username']
-    password = request.form['password']
+        User = db.session.query(models.User).filter(models.User.username == username, models.User.password == password)
+        if User is not None:
+            session['username'] = username
+            flash('login succesful', 'success')
+            return render_template("login.html")
+        else:
+            error = 'wrong username/password'
+            render_template("login.html", error)
 
-    User = db.session.query(models.User).filter(models.User.username == username, models.User.password == password)
-    if User is not None:
-        session['username'] = username
-        return render_template("login.html")
-    return redirect("index.html")
+    return render_template("login.html")
 
 
+@app.route('/logout')
+def logout():
+    session.clear()
+    flash("you've been logged out",'success')
+    return redirect(url_for('login'))
+
+@app.route('/profile')
+def profile():
+    return render_template("profile.html")
 if __name__ == '__main__':
    
 
