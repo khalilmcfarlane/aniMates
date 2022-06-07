a241d75a9f2706e35e0242e1293f73aa4ed40488
Khalil McFarlane <kmac36960@gmail.com>
2022-06-07 15:21:27 -0400

    Created post class and anime class + tested posts

--- app.py
+++ app.py
@@ -1,16 +1,28 @@
+from email import message
 from flask import Flask, redirect, render_template, request, session, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 from random import randint
+from werkzeug.utils import secure_filename
+from datetime import datetime
+from pytz import timezone
+
 import models
 import settings
 
 app = settings.init_flask_app()
 db = SQLAlchemy(app)
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    posts = db.session.query(models.Post).all()
+    return render_template("index.html", posts=posts)
 
 
 @app.route('/signup', methods=['POST', 'GET'])
@@ -34,12 +46,13 @@ def login():
     if "username" in session:
         redirect('/profiles/%s' % session['username'])
         # send to profile page
-    # else login 
+    # else login
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
 
-        User = db.session.query(models.User).filter(models.User.username == username, models.User.password == password)
+        User = db.session.query(models.User).filter(
+            models.User.username == username, models.User.password == password)
         if User is not None:
             session['username'] = username
             flash('login succesful', 'success')
@@ -54,16 +67,63 @@ def login():
 @app.route('/logout')
 def logout():
     session.clear()
-    flash("you've been logged out",'success')
+    flash("you've been logged out", 'success')
     return redirect(url_for('login'))
+
 
 @app.route('/profiles/<username>')
 def profile(username):
     return render_template("profile.html")
 
 
+@app.route('/anime')
+def anime():
+    anime_list = db.session.query(models.Anime).all()
+    return render_template("allAnime.html", anime_list=anime_list)
+
+
+@app.route('/anime/<id>')
+def anime_detail(id):
+    anime = db.session.query(models.Anime).filter(models.Anime.id == id)
+    return render_template("animeDetail.hmtl", anime=anime)
+
+
+@app.route('/create', methods=['GET', 'POST'])
+def create_post():
+    if request.method == 'POST':
+        if 'username' in session:
+            username = session['username']
+            title = request.form['title']
+            text = request.form['text']
+            image = request.files['image']
+
+            if not image:
+                flash("Image Not Uploaded!", "message")
+                return render_template("createPost.html")
+            if allowed_file(image.filename):
+                image.save(secure_filename(image.filename))
+                img = image.filename
+                id = randint(100, 1000000000)
+                user = db.session.query(models.User).filter(
+                    models.User.username == username)
+                user_id = user.id
+                post = models.Post(id, user_id, title, text, img)
+                post.time_sent = datetime.now(timezone('US/Eastern'))
+                db.session.add(post)
+                db.session.commit()
+
+                flash("Post Created!")
+                return redirect('/')
+            elif not allowed_file(image.filename):
+                flash("Incorrect file format: Please upload jpg, png, or gif", "message")
+        else:
+            flash('Please login first!', 'error')
+            return redirect('/login')
+
+    return render_template("createPost.html")
+
+
 if __name__ == '__main__':
-   
 
     db.create_all()
     app.run()
--- models.py
+++ models.py
@@ -10,11 +10,12 @@ db = SQLAlchemy(app)
 jikan = Jikan()
 eastern = timezone('US/Eastern')
 
+
 class User(db.Model):
     __tablename__ = 'users'
-    id = db.Column(db.Integer)
-    username = db.Column(db.String(30), primary_key=True)
-    password = db.Column(db.String(30), primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(30), unique=True)
+    password = db.Column(db.String(30), unique=True)
 
     def __init__(self, username, password):
         self.username = username
@@ -36,6 +37,7 @@ class Anime(db.Model):
         self.year = year
         self.image = image
 
+
 """
 class Message(db.Model):
     __tablename__ = 'messages'
@@ -50,5 +52,39 @@ class Message(db.Model):
         self.recip_id = recip_id
         self.text = text
 """
+
+
+class Post(db.Model):
+    __tablename__ = 'posts'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey(
+        'users.id'), nullable=False, primary_key=True)
+    title = db.Column(db.String())
+    text = db.Column(db.Text, nullable=False)
+    image = db.Column(db.String, nullable=False)
+    time_sent = db.Column(db.DateTime(), default=datetime.now(eastern))
+
+    def __init__(self, id, user_id, title, text, image):
+        self.id = id
+        self.user_id = user_id
+        self.title = title
+        self.text = text
+        self.image = image
+
+"""
+class Image(db.Model):
+    __tablename__ = 'images'
+    id = db.Column(db.Integer, primary_key=True)
+    img = db.Column(db.Text, nullable=False)
+    name = db.Column(db.Text, nullable=False)
+    mime_type = db.Column(db.Text, nullable=False)
+
+    def __init__(self, id, img, name, mime_type):
+        self.id = id
+        self.img = img
+        self.name = name
+        self.mime_type = mime_type
+"""
+
 
 db.create_all()
